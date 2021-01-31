abd346fcbd92373d06bea929bb08dee448634241
root <root@debian10.example.net>
2021-01-31 18:05:43 +0100

    passwd

--- Instalación.sh
+++ Instalación.sh
@@ -8,4 +8,5 @@ sudo cp proftpd.conf /etc/proftpd/proftpd.conf
 sudo cp tls.conf /etc/proftpd/tls.conf
 sudo cp proftpd.key /etc/ssl/private/proftpd.key
 sudo cp proftpd.crt /etc/ssl/certs/proftpd.crt
+sudo cp ftpd.passwd /etc/proftpd/ftpd.passwd
 
